ccc28dd308d88872bb8d55f8bf58ed28ccc73b65
gamzeakyuz <gamz3akyuz@gmail.com>
2025-11-05 15:52:02 +0300

    Initial Commit

---
+++ Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by GamzeAkyuz on 5.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
